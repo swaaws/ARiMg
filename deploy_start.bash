d11b10aed4d8f6bf7d928623b596b4394933c63d
swaaws <40453962+swaaws@users.noreply.github.com>
2021-06-01 22:02:15 +0200

    Update deploy_start.bash

--- deploy_start.bash
+++ deploy_start.bash
@@ -7,10 +7,8 @@ if test "$#" -ne 1; then
     exit
 fi
 
-
-read -p "Chosse Filedesign: [1] - sd_card-image, [2] - plain_rootfs: " filedesign
+read -p "Choose File Design: [1] - sd_card-image, [2] - plain_rootfs: " filedesign
 read -p "want shell access to img? [y] - yes, [n] - no: " shellaccess
-
 
 compression=`file $1 | awk '{print $2}'`
 echo get file info
@@ -23,7 +21,6 @@ if [ -d ~/mnt ]; then
     sudo umount mnt/boot
     sudo umount mnt
     sudo rm -rf ~/mnt
-
 fi
 
 if [ -f ~/spinup.img ]; then
@@ -51,8 +48,6 @@ fi
 echo "$(grep -v "`cat ~/.ssh/reverse_rsa.pub`" ~/.ssh/authorized_keys)" > ~/.ssh/authorized_keys
 cat ~/.ssh/reverse_rsa.pub >> ~/.ssh/authorized_keys
 
-
-
 #sd_card-image
 if [[  $filedesign == 1 ]]; then
     case $compression in
@@ -72,30 +67,22 @@ if [[  $filedesign == 1 ]]; then
             xz --decompress ~/spinup.img.xz
             ;;
 
-        gzip)
-
-            echo TODO: Decompress gzip with img architecture
-            exit 1
-            ;;
-
         *)
             echo unknown compression: $compression
+            echo Open a issue with shell output: https://github.com/swaaws/ARiMg/issues/new
             exit 1
             ;;
     esac
+
     echo Create Mountpoint
     mkdir ~/mnt
-
     startbit=`fdisk -l ~/spinup.img -o start,id,type | grep "83 Linux" | awk '{print $1}'`
+
     echo Start bit for root: $startbit
     sectorsize1=`fdisk -l ~/spinup.img | grep "I/O size" | awk '{print $4}'`
+
     echo Sector Size: $sectorsize1
-
     echo Mountbit: $(($startbit*$sectorsize1))
-
-    #   526336  *    512
-    # Start Bit * Sector Size
-
     echo Mount Image
     sudo mount -o loop,offset=$(($startbit*$sectorsize1)) ~/spinup.img ~/mnt
 
@@ -104,36 +91,34 @@ fi
 
 #plain_rootfs
 if [[  $filedesign == 2 ]]; then
-
-
-    echo Create Mountpoint
+    echo Create mountpoint
     mkdir ~/mnt
 
     echo Create spinup.img
     sudo dd of=~/spinup.img seek=3900M bs=1 count=0 status=none
 
-    echo Create Patition Table
+    echo Create patition table
     sudo parted ~/spinup.img mktable msdos
 
-    echo Create Boot  Patition
+    echo Create boot patition
     sudo parted ~/spinup.img mkpart primary fat32 2048s 257MiB
 
-    echo Create Root Patition
+    echo Create root patition
     sudo parted ~/spinup.img mkpart primary ext4 257MiB 100%
 
-    echo Attach spinup.img to loop Device
+    echo Attach spinup.img to loop device
     loop=`sudo losetup --find --partscan --show spinup.img`
 
-    echo Create vfat on Boot Patition
+    echo Create vfat on boot patition
     sudo mkfs.vfat -F 32 -n BOOT "${loop}p1" > /dev/null
 
-    echo Create ext4 on Root Partition
+    echo Create ext4 on root partition
     sudo mkfs.ext4 -L rootfs "${loop}p2" | grep ,
 
-    echo Mount Root Patition
+    echo Mount root patition
     sudo mount "${loop}p2" ~/mnt/
 
-    echo Mount Boot Patition
+    echo Mount boot patition
     sudo mkdir ~/mnt/boot
     sudo mount "${loop}p1" ~/mnt/boot
     case $compression in
@@ -157,39 +142,18 @@ if [[  $filedesign == 2 ]]; then
             ;;
 
         *)
-            echo unknown compression
-            echo Remove mnt
+            echo unknown compression: $compression
             sudo umount mnt/boot
             sudo umount mnt
             sudo rm -rf ~/mnt
             sudo rm -rf ~/spinup.img
+            echo Open a issue with shell output: https://github.com/swaaws/ARiMg/issues/new
 
             exit 1
             ;;
     esac
-
-
     sync
 fi
-
-
-#--------------------------------------------------------#
-# Raspberry PI 3:
-# 269484032 b3a80ce2979a0841c06831475993cfff80e5614c Ubuntu    ARM64 ubuntu-21.04-preinstalled-server-arm64+raspi.img.xz
-#   4194304 164e1bdd70d743eda3395444a5a5bbdedbab5b04 Debian    ARMHF bone-debian-10.3-iot-armhf-2020-04-06-4gb.img.xz
-#   4194304 8be911fb79156cf4f80bc1451f887883359bca46 Debian    ARMHF bone-eMMC-flasher-debian-10.3-iot-armhf-2020-04-06-4gb.img.xz
-#    532480 f30f9bb907a33736b7d7543da80fac3b1d6c6651 Debian    ARMHF 2021-03-04-raspios-buster-armhf-lite.zip
-#    532480 5e402402984b4f9216b2a3eff1f830d169afd4ea Debian    ARMHF 2021-05-07-raspios-buster-armhf-lite.zip
-#         0 4f0fe7bc9944ca244c3f719da46386200d94a253 ArchLinux ARM64 ArchLinuxARM-rpi-2-latest.tar.gz
-#
-#
-#
-#
-#
-# TODO: offset by sha hash
-#--------------------------------------------------------#
-
-
 
 if [ -d ~/mnt/etc/ ]; then
     echo "Mount Suxxxxxess"
@@ -206,7 +170,6 @@ fi
 
 echo Copy RSA to root fs
 sudo cp ~/.ssh/id_rsa.pub ~/mnt/id_rsa.pub
-
 
 echo Create notifyer.service
 cat <<'EOF' >> ~/notifyer.service
@@ -228,12 +191,9 @@ sudo cp -f ~/.ssh/reverse_rsa ~/mnt/id_rsa
 echo Remove notifyer.service
 rm  ~/notifyer.service
 
-
-
 echo Create netconfig
 cat <<'EOF' >> ~/netconfig
 #!/bin/bash
-
 interface=`ip -o -6 route show to default | awk '{print $5}'`
 echo hostname: `hostname` gateway: `ip -o -6 route show to default \
 | awk '{print $3}'` global6: `ip addr show $interface | grep global \
@@ -243,7 +203,7 @@ echo hostname: `hostname` gateway: `ip -o -6 route show to default \
 | awk '{print $2}'` failed: `systemctl status | grep "  Failed: " | awk '{print $2}'` units
 EOF
 
-echo Copy netconfig to Root fs
+echo Copy netconfig to rootfs
 sudo cp -f ~/netconfig ~/mnt/
 
 echo Remove netconfig
@@ -262,11 +222,9 @@ Added File: /home/spinup/.ssh/authorized_keys
 Set Hostname: "pending-setup" > /etc/hostname
 Systemd Service Enabled: /etc/systemd/system/notifyer.service
 EOF
-echo Copy modification.txt to Root fs
+echo Copy modification.txt to rootfs
 sudo cp -f ~/modification.txt ~/mnt/modification.txt
 rm  ~/modification.txt
-
-
 
 echo Create finish.bash
 cat <<'EOF' >> ~/finish.bash
@@ -276,14 +234,9 @@ rm /netconfig /netconfig_data /notifyer /etc/systemd/system/notifyer.service /ho
 echo "finished-setup" > /etc/hostname
 rm finish.bash
 EOF
-echo Copy finish.bash to Root fs
+echo Copy finish.bash to rootfs
 sudo cp -f ~/finish.bash ~/mnt/finish.bash
 rm  ~/finish.bash
-
-
-
-
-
 
 myip=`ip addr | grep inet6 | grep global |  awk '{print $2}' | rev | cut -c4- | rev`
 myuser=`whoami`
@@ -298,12 +251,11 @@ echo Copy modification.txt to Root fs
 sudo cp -f ~/notifyer ~/mnt/notifyer
 rm  ~/notifyer
 
-
 echo Chroot
 #perl -e 'print crypt("spinup", "salt"),"\n"'
 sudo chroot ~/mnt/ /bin/bash << "EOT"
-useradd -m -s $(which bash) -p saurX9qN91.BQ spinup
 useradd -m -s $(which bash) -p saurX9qN91.BQ -G sudo spinup
+useradd -m -s $(which bash) -p saurX9qN91.BQ spinup
 mkdir -p /home/spinup/.ssh
 cat /id_rsa.pub > /home/spinup/.ssh/authorized_keys
 mv /modification.txt /home/spinup/modification.txt
@@ -325,14 +277,12 @@ echo interactive shell! CTRL-d if done
 bash -c "sudo chroot ~/mnt/"
 fi
 
-
 if [[  $filedesign == 2 ]]; then
 echo Detach loop
 sudo losetup --detach-all
 sudo losetup -d `losetup -a | grep spinup.img | awk -F ":" '{print $1}'`
 echo Unmount mnt / boot
 sudo umount ~/mnt/boot
-
 fi
 
 echo Unmount Image
@@ -341,7 +291,6 @@ sudo umount ~/mnt
 echo Remove Mountpoint
 sudo rm -rf ~/mnt
 
-
 if [ -f ~/$1-spinup.img ]; then
   if [ -f ~/$1-spinup.img.old ]; then
     echo Removed $1-spinup.img.old
@@ -349,7 +298,7 @@ if [ -f ~/$1-spinup.img ]; then
   fi
 mv $1-spinup.img $1-spinup.img.old
 fi
-
 mv spinup.img $1-spinup.img
 
 echo -e "\e[32mFinished, $1-spinup.img created\e[0m"
+echo Run ./deploy_cache.bash
