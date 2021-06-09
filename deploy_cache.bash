ef028bfcdad49482641b93d822e483a45194ed97
swaaws <40453962+swaaws@users.noreply.github.com>
2021-06-09 21:26:49 +0200

    Update deploy_cache.bash

--- deploy_cache.bash
+++ deploy_cache.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 clear
-
+cat pending_store
+echo Hosts in File: `wc -l pending_store | awk '{print $1}'`
 
 loop=true
 while $loop; do
@@ -20,35 +21,17 @@ while $loop; do
     if [ "$trapKey" ] ;then
         printf "\nDoing something with '%s'.\n" $trapKey
     fi
-    echo -n .
-done
-
-
-break
-
-
-
-
-
-
-cat pending_store
-echo Hosts in File: `wc -l pending_store | awk '{print $1}'`
-
-while true; do
-
-  if [ -f ~/pending ]; then
-    cat ~/pending >> ~/pending_store
-    sort ~/pending_store > ~/pending_store.tmp
-    uniq  ~/pending_store.tmp ~/pending_store.now
-    clear
-    cat pending_store.now
-    echo Hosts in File: `wc -l pending_store.now | awk '{print $1}'`
-    rm ~/pending
-    cp ~/pending_store.now ~/pending_store
-    rm ~/pending_store.tmp
-    rm ~/pending_store.now
-  fi
-
-
-sleep 5
+    if [ -f ~/pending ]; then
+      cat ~/pending >> ~/pending_store
+      sort ~/pending_store > ~/pending_store.tmp
+      uniq  ~/pending_store.tmp ~/pending_store.now
+      clear
+      cat pending_store.now
+      echo Hosts in File: `wc -l pending_store.now | awk '{print $1}'`
+      rm ~/pending
+      cp ~/pending_store.now ~/pending_store
+      rm ~/pending_store.tmp
+      rm ~/pending_store.now
+    fi
+    sleep 5
 done
