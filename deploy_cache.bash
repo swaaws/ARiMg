879f1e2db515f47ca9e1e6890b03c2409bcf95d1
swaaws <40453962+swaaws@users.noreply.github.com>
2021-06-09 21:20:30 +0200

    Update deploy_cache.bash

--- deploy_cache.bash
+++ deploy_cache.bash
@@ -1,5 +1,36 @@
 #!/bin/bash
 clear
+
+
+loop=true
+while $loop; do
+    trapKey=
+    if IFS= read -d '' -rsn 1 -t .002 str; then
+        while IFS= read -d '' -rsn 1 -t .002 chr; do
+            str+="$chr"
+        done
+        case $str in
+            $'\E[A') trapKey=UP    ;;
+            $'\E[B') trapKey=DOWN  ;;
+            $'\E[C') trapKey=RIGHT ;;
+            $'\E[D') trapKey=LEFT  ;;
+            q | $'\E') loop=false  ;;
+        esac
+    fi
+    if [ "$trapKey" ] ;then
+        printf "\nDoing something with '%s'.\n" $trapKey
+    fi
+    echo -n .
+done
+
+
+break
+
+
+
+
+
+
 cat pending_store
 echo Hosts in File: `wc -l pending_store | awk '{print $1}'`
 
