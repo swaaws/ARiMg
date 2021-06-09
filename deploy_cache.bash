63b3bd1563d5026dd960397226ce2a1b38f02dfe
swaaws <40453962+swaaws@users.noreply.github.com>
2021-06-09 22:10:44 +0200

    Update deploy_cache.bash

--- deploy_cache.bash
+++ deploy_cache.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 clear
-cat pending_store
-echo Hosts in File: `wc -l pending_store | awk '{print $1}'`
-
+cat ~/pending_store
+echo Hosts in File: `grep -o -i "hostname:" ~/pending_store | wc -l`
+echo Press [q] when the number of hosts is correct.
 loop=true
 while $loop; do
     trapKey=
@@ -21,17 +21,18 @@ while $loop; do
     if [ "$trapKey" ] ;then
         printf "\nDoing something with '%s'.\n" $trapKey
     fi
+    echo -n .
     if [ -f ~/pending ]; then
       cat ~/pending >> ~/pending_store
       sort ~/pending_store > ~/pending_store.tmp
       uniq  ~/pending_store.tmp ~/pending_store.now
+      mv ~/pending_store.now ~/pending_store
       clear
-      cat pending_store.now
-      echo Hosts in File: `wc -l pending_store.now | awk '{print $1}'`
+      cat ~/pending_store
+      echo Hosts in File: `grep -o -i "hostname:" ~/pending_store | wc -l`
+      echo Press [q] when the number of hosts is correct.
       rm ~/pending
-      cp ~/pending_store.now ~/pending_store
       rm ~/pending_store.tmp
-      rm ~/pending_store.now
     fi
     sleep 5
 done
